541be93d1e0fa466fb4b288f1b168edb9a4557e2
eun10e01 <eun10e01@gmail.com>
2026-07-30 20:12:53 +0900

    Update HomeScreen.kt: TodoList, ScheduleEditScreen.kt

--- app/src/main/java/com/example/todoapp/pages/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/todoapp/pages/main/home/HomeScreen.kt
@@ -1,18 +1,27 @@
 package com.example.todoapp.pages.main.home
 
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalRippleConfiguration
+import androidx.compose.material3.RippleConfiguration
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,9 +32,63 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+
+import com.example.todoapp.pages.main.home.ToDoItemData
+
+@Composable
+fun ToDoItemCard(title: String, time: String? = null, modifier: Modifier = Modifier){
+    var isChecked by remember {mutableStateOf(false)}
+
+    Row(modifier = Modifier.fillMaxWidth()){
+        CompositionLocalProvider(LocalRippleConfiguration provides RippleConfiguration(Color(0xFFF2F8E3))){
+            Checkbox(checked = isChecked, onCheckedChange = {isChecked = it},
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color(0xFFBDCEBD),
+                    uncheckedColor = Color.Black,
+                    checkmarkColor = Color.White
+                ),
+                modifier = Modifier.padding(start = 10.dp)
+            )
+        }
+
+        Text(
+            text = title,
+            fontSize = 15.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Medium,
+            color = if (isChecked) Color.Gray else Color.Black,
+            textDecoration = if (isChecked) TextDecoration.LineThrough else TextDecoration.None,
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 11.5.dp)
+        )
+
+        if (time != null) {
+            Text(
+                text = time,
+                fontSize = 15.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Medium,
+                color = if (isChecked) Color.Gray else Color.Black,
+                textDecoration = if (isChecked) TextDecoration.LineThrough else TextDecoration.None,
+                modifier = Modifier
+                    .padding(top = 14.5.dp, end = 30.dp)
+            )
+        }
+    }
+
+    HorizontalDivider(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp),
+        thickness = 0.3.dp,
+        color = Color.Black
+    )
+}
 
 @Composable
 fun CustomProgressBar(progress: Float, modifier: Modifier = Modifier){
@@ -103,6 +166,7 @@ fun HomeScreen() {
         val formatter = SimpleDateFormat("yyyy년 MM월 dd일 EEEE", Locale.KOREAN)
         formatter.format(Date())
     }
+    val todoList = remember{mutableStateListOf<ToDoItemData>()}
 
     Scaffold(){
         innerPadding -> Column(modifier = Modifier
@@ -195,7 +259,33 @@ fun HomeScreen() {
                         .fillMaxSize()
                         .padding(start = 15.dp, end = 15.dp, bottom = 15.dp)
                         .background(color = Color.White, shape = RoundedCornerShape(7.dp))
-                    )
+                    ){
+                        LazyColumn(modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 8.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp)
+                        ){
+                            item{
+                                ToDoItemCard("영어 단어 10개 외우기", "07:00", modifier = Modifier)
+                            }
+
+                            item{
+                                ToDoItemCard("자소서 수정하기", modifier = Modifier)
+                            }
+
+                            item{
+                                ToDoItemCard("뮤지컬 티켓팅 하기", "18:45", modifier = Modifier)
+                            }
+
+                            item{
+                                ToDoItemCard("동아리 회의하기", "23:00", modifier = Modifier)
+                            }
+
+                            items(todoList){ item ->
+                                ToDoItemCard(title = item.title, time = item.time)
+                            }
+                        }
+                    }
                 }
             }
         }
