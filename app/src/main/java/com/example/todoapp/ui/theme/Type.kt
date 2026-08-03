b05896e15b56c01cde54f6effc57ad3c97db4609
SoJeong <misteryoh0308@gmail.com>
2026-08-03 21:35:08 +0900

    Update fontStyle '나눔고딕'

--- app/src/main/java/com/example/todoapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/Type.kt
@@ -1,20 +1,55 @@
 package com.example.todoapp.ui.theme
 
+import com.example.todoapp.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+
+val NanumGothic = FontFamily(
+
+    Font(
+        resId = R.font.nanum_gothic,
+        weight = FontWeight.Normal
+    ),
+
+    Font(
+        resId = R.font.nanum_gothic_bold,
+        weight = FontWeight.Bold
     )
+
+)
+
+val Typography = Typography(
+
+    bodyLarge = TextStyle(
+
+        fontFamily = NanumGothic,
+
+        fontWeight = FontWeight.Normal,
+
+        fontSize = 16.sp,
+
+        lineHeight = 24.sp,
+
+        letterSpacing = 0.5.sp
+
+    )
+
+)
+
+//val Typography = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +66,4 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
+//)
