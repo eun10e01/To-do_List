00f8f7799a11791d6f7dd1f080a4d18dfa5d3220
SoJeong <misteryoh0308@gmail.com>
2026-08-06 18:28:08 +0900

    edit AndroidManifest.xml, build.gradle.kts : add internet permission and Retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
+
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
