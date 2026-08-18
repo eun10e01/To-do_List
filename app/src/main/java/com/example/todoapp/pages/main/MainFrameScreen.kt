9d29353c825ab68b41f5a0862cf8429320833bae
SoJeong <misteryoh0308@gmail.com>
2026-08-19 01:45:55 +0900

    Update ui : complete TopAppBar

--- app/src/main/java/com/example/todoapp/pages/main/MainFrameScreen.kt
+++ app/src/main/java/com/example/todoapp/pages/main/MainFrameScreen.kt
@@ -3,6 +3,8 @@ package com.example.todoapp.pages.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -15,12 +17,15 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import com.example.todoapp.navigation.Screen
 import com.example.todoapp.navigation.bottomNavItems
 import com.example.todoapp.navigation.AppNavigation
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainFrameScreen(){
     val navController = rememberNavController()
@@ -29,7 +34,44 @@ fun MainFrameScreen(){
 
     val bottomBarRoutes = listOf(Screen.Calendar.route, Screen.Home.route, Screen.MyPage.route)
 
+    val topBarTitles = mapOf(
+        Screen.ChangeUserInfo.route to "회원정보 변경",
+        Screen.ChangeNickname.route to "닉네임 변경",
+        Screen.ChangeEmail.route to "이메일 변경",
+        Screen.ChangePhoneNumber.route to "휴대폰번호 변경",
+        Screen.ChangeDateOfBirth.route to "생년월일 변경",
+        Screen.ChangePassword.route to "비밀번호 변경",
+        Screen.Withdrawal.route to "회원 탈퇴"
+    )
+
     Scaffold(
+        topBar = {
+            topBarTitles[currentRoute]?.let { title ->
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = title,
+                            fontSize = 18.sp,
+//                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                navController.popBackStack()
+                            }
+                        ) {
+                            Icon(
+                                imageVector =
+                                    Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "뒤로가기"
+                            )
+                        }
+                    }
+                )
+            }
+        },
+
         bottomBar = {
             if(currentRoute in bottomBarRoutes){
                 NavigationBar(modifier = Modifier
